0576ff5a41cddc19c6922e016b6d2ddad996fe5a
Neil Green <neil@melessoftware.com>
2013-05-29 11:37:04 +0100

    fix up build

--- src/main/java/com/ft/api/content/items/v1/services/bodyprocessing/BodyProcessingException.java
+++ src/main/java/com/ft/api/content/items/v1/services/bodyprocessing/BodyProcessingException.java
@@ -1,8 +1,6 @@
 package com.ft.api.content.items.v1.services.bodyprocessing;
 
-import com.ft.api.utils.exception.UnexpectedException;
-
-public class BodyProcessingException extends UnexpectedException {
+public class BodyProcessingException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/ft/api/content/items/v1/services/bodyprocessing/xml/eventhandlers/UnexpectedElementStructureException.java
+++ src/main/java/com/ft/api/content/items/v1/services/bodyprocessing/xml/eventhandlers/UnexpectedElementStructureException.java
@@ -1,8 +1,6 @@
 package com.ft.api.content.items.v1.services.bodyprocessing.xml.eventhandlers;
 
-import com.ft.api.utils.exception.ExpectedException;
-
-public class UnexpectedElementStructureException extends ExpectedException {
+public class UnexpectedElementStructureException extends RuntimeException {
 
     private static final long serialVersionUID = 5763609104620978952L;
 
