ba8d6c4f4c90a52cae2b759fa09f5c4873dc87fe
Sandor Torok <sandor.torok@ft.com>
2014-09-01 18:06:47 +0300

    P089 US38786 TA86042 - Image ID is now extracted from the class attribute (having pattern wp-image-XXXX) if the data-img-id attribute is missing on <img>

--- src/test/java/com/ft/content/bodyprocessing/xml/eventhandlers/BlogPostImageRetainWithSpecificAttributesXMLEventHandlerTest.java
+++ src/test/java/com/ft/content/bodyprocessing/xml/eventhandlers/BlogPostImageRetainWithSpecificAttributesXMLEventHandlerTest.java
@@ -12,13 +12,15 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.events.StartElement;
 import java.util.Map;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BlogPostImageRetainWithSpecificAttributesXMLEventHandlerTest extends BaseXMLEventHandlerTest {
-    
+
     @Mock
     private BodyProcessingContext bodyProcessingContext;
     @Mock
@@ -26,12 +28,14 @@ public class BlogPostImageRetainWithSpecificAttributesXMLEventHandlerTest extend
     @Mock
     private XMLEventReader eventReader;
 
+    private StartElement startElement;
+
     private BlogPostImageRetainWithSpecificAttributesXMLEventHandler
             blogPostImageRetainWithSpecificAttributesXMLEventHandler;
 
     @Before
     public void setUp() {
-        blogPostImageRetainWithSpecificAttributesXMLEventHandler = new 
+        blogPostImageRetainWithSpecificAttributesXMLEventHandler = new
                 BlogPostImageRetainWithSpecificAttributesXMLEventHandler();
     }
 
@@ -43,6 +47,93 @@ public class BlogPostImageRetainWithSpecificAttributesXMLEventHandlerTest extend
         blogPostImageRetainWithSpecificAttributesXMLEventHandler.handleStartElementEvent
                 (getStartElementWithAttributes("img", expectedImageAttributes), eventReader,
                 eventWriter, bodyProcessingContext);
+
+        verify(bodyProcessingContext).addImageWithAttributes(expectedImageAttributes, TypeBasedImage.ImageType.INLINE_EXT);
+        verify(eventWriter).writeStartTag("img", expectedImageAttributes);
+    }
+
+    @Test
+    public void imageIdShouldBeExtractedFromClassAttributeIfNotPresentOnImgClass() throws Exception {
+        Map<String, String> expectedImageAttributes = ImmutableMap
+                .of(ImageAttribute.SRC.getAttributeName(), "someImage.jpg", ImageAttribute.ALT.getAttributeName(),
+                        "someAlt", ImageAttribute.IMAGE_ID.getAttributeName(), "someId");
+
+
+        startElement = getStartElementWithAttributes("img", ImmutableMap.of(ImageAttribute.SRC.getAttributeName(),
+                "someImage.jpg", ImageAttribute.ALT.getAttributeName(), "someAlt", "class", "wp-image-someId"));
+        when(eventReader.hasNext()).thenReturn(true);
+
+        blogPostImageRetainWithSpecificAttributesXMLEventHandler.handleStartElementEvent(startElement, eventReader, eventWriter, bodyProcessingContext);
+
+        verify(bodyProcessingContext).addImageWithAttributes(expectedImageAttributes, TypeBasedImage.ImageType.INLINE_EXT);
+        verify(eventWriter).writeStartTag("img", expectedImageAttributes);
+    }
+
+    @Test
+    public void imageIdShouldBeExtractedFromMultipleClassAttributeIfNotPresentOnImgClass() throws Exception {
+        Map<String, String> expectedImageAttributes = ImmutableMap
+                .of(ImageAttribute.SRC.getAttributeName(), "someImage.jpg", ImageAttribute.ALT.getAttributeName(),
+                        "someAlt", ImageAttribute.IMAGE_ID.getAttributeName(), "someId");
+
+
+        startElement = getStartElementWithAttributes("img", ImmutableMap.of(ImageAttribute.SRC.getAttributeName(),
+                "someImage.jpg", ImageAttribute.ALT.getAttributeName(), "someAlt",
+                "class", "someOtherClass wp-image-someId"));
+        when(eventReader.hasNext()).thenReturn(true);
+
+        blogPostImageRetainWithSpecificAttributesXMLEventHandler.handleStartElementEvent(startElement, eventReader, eventWriter, bodyProcessingContext);
+
+        verify(bodyProcessingContext).addImageWithAttributes(expectedImageAttributes, TypeBasedImage.ImageType.INLINE_EXT);
+        verify(eventWriter).writeStartTag("img", expectedImageAttributes);
+    }
+
+    @Test
+    public void imageIdShouldNotBeAddedToTheContextIfNotPresentAtAll() throws Exception {
+        Map<String, String> expectedImageAttributes = ImmutableMap
+                .of(ImageAttribute.SRC.getAttributeName(), "someImage.jpg", ImageAttribute.ALT.getAttributeName(),
+                        "someAlt");
+
+
+        startElement = getStartElementWithAttributes("img", ImmutableMap.of(ImageAttribute.SRC.getAttributeName(),
+                "someImage.jpg", ImageAttribute.ALT.getAttributeName(), "someAlt"));
+        when(eventReader.hasNext()).thenReturn(true);
+
+        blogPostImageRetainWithSpecificAttributesXMLEventHandler.handleStartElementEvent(startElement, eventReader, eventWriter, bodyProcessingContext);
+
+        verify(bodyProcessingContext).addImageWithAttributes(expectedImageAttributes, TypeBasedImage.ImageType.INLINE_EXT);
+        verify(eventWriter).writeStartTag("img", expectedImageAttributes);
+    }
+
+    @Test
+    public void imageIdShouldNotBeExtractedFromClassAttributeIfImageIdAttributeIsPresent() throws Exception {
+        Map<String, String> expectedImageAttributes = ImmutableMap
+                .of(ImageAttribute.SRC.getAttributeName(), "someImage.jpg", ImageAttribute.ALT.getAttributeName(),
+                        "someAlt", ImageAttribute.IMAGE_ID.getAttributeName(), "someId");
+
+
+        startElement = getStartElementWithAttributes("img", ImmutableMap.of(ImageAttribute.SRC.getAttributeName(),
+                "someImage.jpg", ImageAttribute.ALT.getAttributeName(), "someAlt",
+                ImageAttribute.IMAGE_ID.getAttributeName(), "someId", "class", "wp-image-someOtherId"));
+        when(eventReader.hasNext()).thenReturn(true);
+
+        blogPostImageRetainWithSpecificAttributesXMLEventHandler.handleStartElementEvent(startElement, eventReader, eventWriter, bodyProcessingContext);
+
+        verify(bodyProcessingContext).addImageWithAttributes(expectedImageAttributes, TypeBasedImage.ImageType.INLINE_EXT);
+        verify(eventWriter).writeStartTag("img", expectedImageAttributes);
+    }
+
+    @Test
+    public void imageIdShouldNotBeExtractedFromClassAttributeIfPatternIsNotMatching() throws Exception {
+        Map<String, String> expectedImageAttributes = ImmutableMap
+                .of(ImageAttribute.SRC.getAttributeName(), "someImage.jpg", ImageAttribute.ALT.getAttributeName(),
+                        "someAlt");
+
+
+        startElement = getStartElementWithAttributes("img", ImmutableMap.of(ImageAttribute.SRC.getAttributeName(),
+                "someImage.jpg", ImageAttribute.ALT.getAttributeName(), "someAlt", "class", "someClassAttribute"));
+        when(eventReader.hasNext()).thenReturn(true);
+
+        blogPostImageRetainWithSpecificAttributesXMLEventHandler.handleStartElementEvent(startElement, eventReader, eventWriter, bodyProcessingContext);
 
         verify(bodyProcessingContext).addImageWithAttributes(expectedImageAttributes, TypeBasedImage.ImageType.INLINE_EXT);
         verify(eventWriter).writeStartTag("img", expectedImageAttributes);
